5e32f8ffe419206cc513cfbec89c7b588da8e8cf
hiba <hibamokbali@gmail.com>
2025-11-24 02:14:23 +0100

    Amélioration du footer

--- src/app/user/acceuil/acceuil.ts
+++ src/app/user/acceuil/acceuil.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-acceuil',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './acceuil.html',
   styleUrl: './acceuil.css',
 })
